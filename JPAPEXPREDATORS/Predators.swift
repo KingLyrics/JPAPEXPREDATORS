45442524f2eed594af68996426715bb6dae07f6e
aBugInTheSystem <ekom338.com@gmail.com>
2024-03-14 13:51:55 +0300

    Decode class done.

--- JPAPEXPREDATORS/Predators.swift
+++ JPAPEXPREDATORS/Predators.swift
@@ -10,4 +10,21 @@ import Foundation
 class Predators{
     
     var apexPreadators:[ApexPredator] = []
+    
+    init(){
+        DecodeApexPreadatorData()
+    }
+    
+    func DecodeApexPreadatorData(){
+        if let url = Bundle.main.url(forResource: "jpapexpredators", withExtension: "json"){
+            do{
+                let data = try Data(contentsOf: url)
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                apexPreadators = try decoder.decode([ApexPredator].self, from: data)
+            }catch{
+                print("Error decoding json data: \(error)")
+            }
+        }
+    }
 }
