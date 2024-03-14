474b5f7cc12ca7eb694881377179a0acec820f5b
aBugInTheSystem <ekom338.com@gmail.com>
2024-03-14 14:52:44 +0300

    Search function implemented.

--- JPAPEXPREDATORS/ContentView.swift
+++ JPAPEXPREDATORS/ContentView.swift
@@ -9,38 +9,73 @@ import SwiftUI
 
 struct ContentView: View {
     let predators = Predators()
+    
+    @State var searchText: String = ""
+    
+    var filteredDinos: [ApexPredator] {
+        if searchText.isEmpty {
+            return predators.apexPreadators
+        } else {
+            return predators.apexPreadators.filter {
+                predator in
+                predator.name.localizedCaseInsensitiveContains(searchText)
+            }
+        }
+    }
+    
     var body: some View {
-        List(predators.apexPreadators){ predator in
-            VStack(alignment:.leading) {
-                HStack{
-                    //Dino image
-                    Image(predator.image)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: 100)
-                        .shadow(color: .white, radius: 1)
-                    
-                    VStack(alignment:.leading){
-                        //Name
-                        Text(predator.name)
-                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
-                        //Type
-                        Text(predator.type.rawValue.capitalized)
-                            .font(.subheadline)
-                            .fontWeight(.semibold)
-                            .padding(.horizontal,13)
-                            .padding(.vertical, 5)
-                            .background(predator.type.background)
-                            .clipShape(.capsule)
-                            
-                        
+        NavigationStack {
+            List {
+                if filteredDinos.isEmpty {
+                    Text("Dino Not Found")
+                        .foregroundColor(.gray)
+                        .fontWeight(.semibold)
+                        .padding(.vertical)
+                } else {
+                    ForEach(filteredDinos) { predator in
+                        VStack(alignment: .leading) {
+                            NavigationLink {
+                                Image(predator.image)
+                                    .resizable()
+                                    .scaledToFit()
+                            } label: {
+                                HStack {
+                                    // Dino image
+                                    Image(predator.image)
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 100, height: 100)
+                                        .shadow(color: .white, radius: 1)
+                                    
+                                    VStack(alignment: .leading) {
+                                        // Name
+                                        Text(predator.name)
+                                            .fontWeight(.bold)
+                                        
+                                        // Type
+                                        Text(predator.type.rawValue.capitalized)
+                                            .font(.subheadline)
+                                            .fontWeight(.semibold)
+                                            .padding(.horizontal, 13)
+                                            .padding(.vertical, 5)
+                                            .background(predator.type.background)
+                                            .clipShape(.capsule)
+                                    }
+                                }
+                            }
+                        }
                     }
                 }
             }
+            .navigationTitle("Apex Predators")
+            .searchable(text: $searchText)
+            .autocorrectionDisabled()
+            .animation(.default, value: searchText)
         }
         .preferredColorScheme(.dark)
     }
 }
+
 
 #Preview {
     ContentView()
