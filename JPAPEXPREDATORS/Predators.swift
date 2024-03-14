772ce3042b17230162828d0ddd4a8f7d8d3bca80
aBugInTheSystem <ekom338.com@gmail.com>
2024-03-14 13:38:56 +0300

    Apex model added. Working on Preadator class.

---
+++ JPAPEXPREDATORS/Predators.swift
@@ -0,0 +1,13 @@
+//
+//  Predators.swift
+//  JPAPEXPREDATORS
+//
+//  Created by Ekomobong Edeme on 14/03/2024.
+//
+
+import Foundation
+
+class Predators{
+    
+    var apexPreadators:[ApexPredator] = []
+}
