3007a1fdb5a47053474782a20c3e7cc4003924e1
aBugInTheSystem <ekom338.com@gmail.com>
2024-03-14 12:57:36 +0300

    Initial Commit

---
+++ JPAPEXPREDATORS/JPAPEXPREDATORSApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPAPEXPREDATORSApp.swift
+//  JPAPEXPREDATORS
+//
+//  Created by Ekomobong Edeme on 14/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct JPAPEXPREDATORSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
